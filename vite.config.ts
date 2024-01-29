d2757c7bc79f9904ddcf7297e3767f0d7a565f2a
Tony Babintsev <tbabintsev2@gmail.com>
2024-01-29 15:05:14 +0200

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dotenv from "dotenv";
+
+const env = dotenv.config().parsed;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +16,5 @@ export default defineConfig({
       },
     ],
   },
+  base: env.VITE_BASE_URL,
 });
